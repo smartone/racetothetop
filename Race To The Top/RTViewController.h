38027008d2391f279eb91a27c034f6c040bd1dba
gary g <tintin2000@yahoo.com>
2013-11-14 10:53:55 -0500

    Initial Commit

---
+++ Race To The Top/RTViewController.h
@@ -0,0 +1,13 @@
+//
+//  RTViewController.h
+//  Race To The Top
+//
+//  Created by me on 11/14/13.
+//  Copyright (c) 2013 me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RTViewController : UIViewController
+
+@end
---
+++ Race To The Top/RTViewController.m
@@ -0,0 +1,29 @@
+//
+//  RTViewController.m
+//  Race To The Top
+//
+//  Created by me on 11/14/13.
+//  Copyright (c) 2013 me. All rights reserved.
+//
+
+#import "RTViewController.h"
+
+@interface RTViewController ()
+
+@end
+
+@implementation RTViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
